8c19aaadf68cd1bf9281bc65194e7f616fd3349c
ninjaCNHH <ninja.cnhh@gmail.com>
2023-09-08 19:07:23 -0700

    In Progress.

---
+++ Hk - FinalBlackBeltProject/Assets/Scripts/ZombieScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.AI;
+using UnityEngine.UI; 
+
+public class ZombieScript : MonoBehaviour
+{
+    [Header("Zombie Traits")] 
+    public int Health;
+    public int Defence;
+    public int Speed;
+
+    [Header("AI Movement")]
+    public NavMeshAgent agent;
+    public Transform alliedBase;
+    public Animation ZombieWalk;
+
+    [Header("UI")]
+    public int AllyHealth; 
+    public Text BaseHealth; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AllyHealth = 100; 
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = alliedBase.position; 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Base"))
+        {
+            AllyHealth -= 20; 
+        }
+    }
+}
